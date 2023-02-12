f7201bfce57dfc9c708e822ed56972a849cecfcc
anupam-ahi <anupam.gollapalli@gmail.com>
2023-02-12 15:22:43 -0800

    added comments to leetcode 56

--- leetcode_56.py
+++ leetcode_56.py
@@ -1,7 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # assume that the minimum selling price of stock is the first item in prices
         _min = prices[0]
-        _max = 0
+        _max = 0  # assume that the maximum profit is 0
         for i in prices:
             profit = i - _min
             _max = max(_max, profit)
